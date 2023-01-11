6ea86400b8610ac688dcbe84b093bb5cf316e4ab
Shagun <shagunchahar8@gmail.com>
2023-01-11 14:00:40 +0530

    first part done

--- app/src/main/java/com/example/news_app/MainActivity.kt
+++ app/src/main/java/com/example/news_app/MainActivity.kt
@@ -2,17 +2,9 @@ package com.example.news_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Adapter
-import android.widget.TableLayout
-import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager.widget.ViewPager
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
-import com.google.android.material.tabs.TabLayout.Tab
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -20,35 +12,24 @@ import retrofit2.Response
 class MainActivity : AppCompatActivity() {
 
     lateinit var newrecyclerview : RecyclerView
-
     lateinit var adapter : ADPATER
     var pagenumber = 1
     var totalresults = -1
     var articles = mutableListOf<DataClass>()
-    var india = findViewById<TextView>(R.id.India)
-    var home = findViewById<TextView>(R.id.home)
-    var tech = findViewById<TextView>(R.id.Tech)
-    var sports = findViewById<TextView>(R.id.Sports)
-    var buisness = findViewById<TextView>(R.id.Buisness)
-    var science = findViewById<TextView>(R.id.Science)
-    lateinit var tabLayout: TabLayout
-    lateinit var viewPager : ViewPager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val fragmentManager = supportFragmentManager
-        tabLayout = findViewById(R.id.taskbar)
-        viewPager.adapter = PAGEADPATER(fragmentManager)
-        tabLayout.setupWithViewPager(viewPager)
+        replaceFragment(news_1())
     }
 
-   /* private fun replaceFragment(fragment: Fragment) {
+    private fun replaceFragment(fragment: Fragment) {
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.frame,fragment)
         fragmentTransaction.commit()
-    }*/
+    }
     private fun getData() {
         RetrofitApi.apiInterface.getData(pagenumber).enqueue(object : Callback<News?> {
             override fun onResponse(
